f5c9ac7c306f5f1b9ad437c924ae097712a3f3c7
Rachit Gandhi <77197545+Rachit-Gandhi@users.noreply.github.com>
2024-10-12 21:55:26 +0530

    data2

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 MODEL_FILE = os.path.join(BASE_DIR, 'wind_speed_lstm_model_retrained.h5')
-DATA_FILE = r'' #PATH TO DATA
+DATA_FILE = r'scaled_turbine1.csv' #PATH TO DATA
 COLUMN_NAME = 'Wind speed (m/s)'
